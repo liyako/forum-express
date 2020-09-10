bcbb871176854ec764cb0bb8093a0fad10c65a1a
cotool <tusky.saga@gmail.com>
2020-09-10 23:26:52 +0800

    Add feature: signInPage, signIn and logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
@@ -29,6 +30,20 @@ const userController = {
         }
       })
     }
+  },
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 
