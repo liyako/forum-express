58e8d30d29e7ca2918639c52c4cc27e702dced01
cotool <tusky.saga@gmail.com>
2020-09-10 23:14:47 +0800

    install bcryptjs body parser and add signup page

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db = require('./models') // 引入資料庫
+const db = require('./models')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+// 設定 view engine 使用 handlebars
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}!`)
+  db.sequelize.sync() // 跟資料庫同步
+  console.log(`Example app listening on port ${port}`)
 })
-
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
