dc302180bafcca7afe606a1cff9d292d20b19f55
cotool <tusky.saga@gmail.com>
2020-09-10 23:20:10 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -9,7 +11,15 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
-
+// setup session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 app.listen(port, () => {
   db.sequelize.sync() // 跟資料庫同步
   console.log(`Example app listening on port ${port}`)
