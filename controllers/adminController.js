8635c3c27232a2e11f7d3f9790aa5c067bd5461c
cotool <tusky.saga@gmail.com>
2020-09-11 03:30:26 +0800

    Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,10 +3,31 @@ const Restaurant = db.Restaurant
 
 const adminController = {
     getRestaurants: (req, res) => {
-      return Restaurant.findAll({raw: true}).then(restaurant => {
-        return res.render('admin/restaurants',{restaurant: restaurant})
+      return Restaurant.findAll({raw: true}).then(restaurants => {
+        return res.render('admin/restaurants',{restaurants: restaurants})
       })
-    }
+    },
+    //新增餐廳
+    createRestaurant: (req,res) => {
+      return res.render('admin/create')
+    },
+    postRestaurant: (req, res) => {
+      if (!req.body.name) {
+        req.flash('error_messages', "name didn't exist")
+        return res.redirect('back')
+      }
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description
+      })
+        .then((restaurant) => {
+          req.flash('success_messages', 'restaurant was successfully created')
+          res.redirect('/admin/restaurants')
+        })
+    },
   }
   
   module.exports = adminController
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('Restaurant', {
+  const Restaurant = sequelize.define('Restaurant', {
     name: DataTypes.STRING,
     tel: DataTypes.STRING,
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT
   }, {});
-  User.associate = function(models) {
+  Restaurant.associate = function(models) {
     // associations can be defined here
   };
-  return User;
+  return Restaurant;
 };
