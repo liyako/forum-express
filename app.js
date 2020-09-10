bae4600f5b3641d72df279e997936cab152c8203
cotool <tusky.saga@gmail.com>
2020-09-11 02:05:58 +0800

    Add authentication before rendering pages

--- app.js
+++ app.js
@@ -12,21 +12,27 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+
 // setup session and flash
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+
 // setup passport
 app.use(passport.initialize())
 app.use(passport.session())
+
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
     res.locals.success_messages = req.flash('success_messages')
     res.locals.error_messages = req.flash('error_messages')
+    res.locals.user = req.user
     next()
 })
+// 跟資料庫同步
 app.listen(port, () => {
-  db.sequelize.sync() // 跟資料庫同步
+  db.sequelize.sync() 
   console.log(`Example app listening on port ${port}`)
 })
+
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app, passport)
