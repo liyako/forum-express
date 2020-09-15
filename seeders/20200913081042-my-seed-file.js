8373d41b593251ed88fdbd8e93f595d85a23292d
cotool <tusky.saga@gmail.com>
2020-09-16 01:32:32 +0800

    Add category seed data

--- seeders/20200913081042-my-seed-file.js
+++ seeders/20200913081042-my-seed-file.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const faker = require('faker')
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    //使用者
     queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
@@ -25,7 +26,18 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
-
+    //category seed data
+    queryInterface.bulkInsert('Categories',
+    ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      .map((item, index) =>
+        ({
+          id: index + 1,
+          name: item,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {})
+    //餐廳
     return queryInterface.bulkInsert('Restaurants', 
       Array.from({length: 50}).map(d =>
       ({
@@ -36,11 +48,13 @@ module.exports = {
         image: faker.image.imageUrl(),
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 5) + 1
       })
     ), {});
   },
   down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Categories',null,{});
     queryInterface.bulkDelete('Users', null, {});
     return queryInterface.bulkDelete('Restaurants', null, {});
   }
