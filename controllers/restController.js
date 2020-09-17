731867a735ed34595fa9da28849b969701a01572
cotool <tusky.saga@gmail.com>
2020-09-17 21:28:27 +0800

    add restaurant feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,22 @@
-const restController = {
-    getRestaurants: (req, res) => {
-      return res.render('restaurants')
-    }
+const db = require('../models') 
+const Restaurant = db.Restaurant
+const Category = db.Category
+
+
+let restController = {
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({include:Category})
+    .then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0,50),
+        categoryName: r.Category.name
+      }))
+      console.log(restaurants)
+      return res.render('restaurants',{
+        restaurants: data
+      })
+    })
+  },
   }
   module.exports = restController
