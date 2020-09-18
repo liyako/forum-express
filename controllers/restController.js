db6e45889468e3b089e7a35e76b6cfb581726bee
cotool <tusky.saga@gmail.com>
2020-09-19 01:54:09 +0800

    Add Convert time format using moment

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models') 
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 
 //分頁
 const pageLimit = 10
@@ -45,8 +47,12 @@ let restController = {
   //顯示單一資料
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id,{
-      include:Category
+      include:[
+        Category,
+        {model:Comment, include:[User]}
+      ]
     }).then(restaurant => {
+      console.log(restaurant.Comments[0].dataValues)
       return res.render('restaurant',{
         restaurant: restaurant.toJSON()
       })
