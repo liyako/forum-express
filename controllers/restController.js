5b7edc6816da2acb1d65d2db7c0708c17c9546c9
cotool <tusky.saga@gmail.com>
2020-09-18 18:36:19 +0800

    add page on index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,19 +2,31 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+//分頁
+const pageLimit = 10
+
 
 let restController = {
   //index
   getRestaurants: (req, res) => {
     let whereQuery ={}
     let categoryId =''
+    let offset = 0
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit 
+    }
     if(req.query.categoryId){
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
-    return Restaurant.findAll({include:Category,where: whereQuery})
-    .then(restaurants => {
-      const data = restaurants.map(r => ({
+    return Restaurant.findAndCountAll({include:Category,where: whereQuery,offset:offset,pageLimit:pageLimit})
+    .then(result => {
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({length:pages}).map((item,index) => index +1)
+      let prev = page -1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0,50),
         categoryName: r.Category.name
@@ -24,6 +36,7 @@ let restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories: categories,
+          page:page,totalPage:totalPage,prev:prev,next:next,
           categoryId: categoryId
         })
       })
