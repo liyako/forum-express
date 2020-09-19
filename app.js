303a066c27c70467a9f24c9f986c222eecf55887
cotool <tusky.saga@gmail.com>
2020-09-19 22:28:47 +0800

    [修改]app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(methodOverride('_method'))
 app.use('/upload', express.static(__dirname + '/upload'))
 // 跟資料庫同步
 app.listen(port, () => {
-  db.sequelize.sync() 
+  //db.sequelize.sync() 
   console.log(`Example app listening on port ${port}`)
 })
 
