699014f1c39794e6070b9234d2489e75b5e3f7d8
cotool <tusky.saga@gmail.com>
2020-09-24 18:26:17 +0800

    refactor adminController.delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -134,12 +134,10 @@ const adminController = {
     },
     //刪除
     deleteRestaurant: (req, res) => {
-      return Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        restaurant.destroy()
-        .then((restaurant) => {
+      adminService.deleteRestaurant(req,res,(data) => {
+        if (data['status'] === 'success'){
           res.redirect('/admin/restaurants')
-        })
+        }
       })
     },
     //瀏覽所有使用者
--- routes/apis.js
+++ routes/apis.js
@@ -9,5 +9,7 @@ const categoryController = require('../controllers/api/categoryController.js')
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
+//delete
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
